1a9974d9788cf0c0ac8729592e6ec036b0c9fd35
Grant Forrest <grant@butlr.io>
2024-03-30 12:52:14 -0400

    upgrade tiptap

---
+++ src/components/richEditor/RichEditor.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { RichEditor } from './RichEditor.js';
+
+const meta = {
+	title: 'RichEditor',
+	component: RichEditor,
+	argTypes: {},
+	parameters: {
+		controls: { expanded: true },
+	},
+} satisfies Meta<typeof RichEditor>;
+
+export default meta;
+
+type Story = StoryObj<typeof RichEditor>;
+
+export const Default: Story = {};
