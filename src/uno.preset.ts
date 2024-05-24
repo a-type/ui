d5ddea08463faba3e25cd4edf46a69022a467e79
Grant Forrest <grant@butlr.io>
2024-05-24 15:25:47 -0400

    fix quotes

--- src/uno.preset.ts
+++ src/uno.preset.ts
@@ -736,7 +736,7 @@ export default function presetAglio({
 
         @font-face {
           font-family: "Inter";
-          src: url(${interFontLocation}) format("truetype-variations");
+          src: url("${interFontLocation}") format("truetype-variations");
           font-weight: 1 999;
           font-style: oblique 0deg 5deg;
           font-display: block;
