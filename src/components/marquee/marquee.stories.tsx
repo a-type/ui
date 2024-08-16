d1af87f4fdaf9f1745ef7abee9d2b86fedf75d0d
Grant Forrest <gaforres@gmail.com>
2024-08-16 16:37:06 -0400

    add marquee component

---
+++ src/components/marquee/marquee.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Marquee } from './marquee.js';
+
+const meta = {
+	title: 'Marquee',
+	component: Marquee,
+	argTypes: {},
+	parameters: {
+		controls: { expanded: true },
+	},
+} satisfies Meta<typeof Marquee>;
+
+export default meta;
+
+type Story = StoryObj<typeof Marquee>;
+
+export const Default: Story = {
+	render(params) {
+		return (
+			<Marquee {...params}>
+				<Marquee.Item>
+					<img src="https://resources.biscuits.club/images/pashka.jpg" />
+				</Marquee.Item>
+				<Marquee.Item className="flex items-center justify-center bg-primary-wash color-primary-dark text-lg">
+					Hello, world
+				</Marquee.Item>
+				<Marquee.Item asChild>
+					<img src="https://resources.biscuits.club/images/pashka.jpg" />
+				</Marquee.Item>
+			</Marquee>
+		);
+	},
+};
