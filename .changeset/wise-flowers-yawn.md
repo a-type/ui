edc6c3e341e551a2b764b414b132c1b4877e2ac4
Grant Forrest <gaforres@gmail.com>
2024-11-12 10:48:58 -0500

    wrap tabs

---
+++ .changeset/wise-flowers-yawn.md
@@ -0,0 +1,5 @@
+---
+'@a-type/ui': patch
+---
+
+Tabs wrap
--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -5,12 +5,12 @@ export const TabsRoot = withClassName(TabsPrimitive.Root, '');
 
 export const TabsList = withClassName(
 	TabsPrimitive.List,
-	'flex flex-row py-2 px-2 justify-center items-start gap-2',
+	'flex flex-row flex-wrap py-2 px-2 items-start gap-2',
 );
 
 export const TabsTrigger = withClassName(
 	TabsPrimitive.Trigger,
-	'layer-components:(flex flex-row items-center justify-center gap-2 color-black py-1 px-4 bg-wash text-md min-w-100px rounded-full border border-1 font-semibold text-gray-7 border-gray-7 border-solid transition-colors cursor-pointer select-none font-sans)',
+	'layer-components:(flex flex-row items-center justify-center gap-2 color-black py-1 px-5 bg-wash text-md min-w-100px rounded-full border border-1 font-semibold text-gray-7 border-gray-7 border-solid transition-colors cursor-pointer select-none font-sans flex-shrink-0)',
 	'hover:bg-primary-light focus-visible:(focus-ring focus-ring-primary-dark outline-off bg-primary-light border-primary-dark) [&[data-state=active]]:(font-semibold bg-primary-light border-primary-light text-black cursor-default hover:bg-primary-light relative z-1)',
 );
 
