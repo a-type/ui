ff73fd1f7b9b808a4407b55466150bc47933826f
Grant Forrest <gaforres@gmail.com>
2024-11-12 21:10:05 -0500

    support vkeyboard with now playing

--- src/components/layouts/PageNowPlaying.tsx
+++ src/components/layouts/PageNowPlaying.tsx
@@ -6,8 +6,12 @@ import { HTMLAttributes } from 'react';
 export function PageNowPlaying({
 	className,
 	unstyled,
+	keepAboveKeyboard,
 	...props
-}: HTMLAttributes<HTMLDivElement> & { unstyled?: boolean }) {
+}: HTMLAttributes<HTMLDivElement> & {
+	unstyled?: boolean;
+	keepAboveKeyboard?: boolean;
+}) {
 	return (
 		<div
 			{...props}
@@ -15,11 +19,14 @@ export function PageNowPlaying({
 				'layer-components:(fixed left-0 right-0 z-now-playing flex flex-col gap-2 items-end)',
 				// on mobile, this must be positioned above any nav bar that's present,
 				// or at minimum in the safe area
-				'layer-comopnents:bottom-[var(--nav-height,env(safe-area-inset-bottom,0px))]',
+				'layer-components:bottom-[var(--nav-height,env(safe-area-inset-bottom,0px))]',
+				'layer-components:transition-bottom',
 				'layer-components:sm:(fixed bottom-3 left-[var(--content-left,20%)] transition-opacity top-auto items-end w-[var(--content-width,100%)] max-w-80vw p-0 opacity-[var(--content-ready,0)])',
 				unstyled
 					? 'layer-variants:p-2'
 					: 'layer-components:(bg-wash p-2px rounded-xl border-light shadow-md min-w-32px items-center justify-center m-2 w-auto)',
+				keepAboveKeyboard &&
+					'layer-variants:lt-sm:bottom-[max(var(--mock-virtual-keyboard-height,env(keyboard-inset-height,0px),var(--nav-height,env(safe-area-inset-bottom,0px))))]',
 				className,
 			)}
 		/>
--- src/components/layouts/layouts.stories.tsx
+++ src/components/layouts/layouts.stories.tsx
@@ -12,6 +12,8 @@ import {
 } from '../navBar.js';
 import { Icon } from '../icon.js';
 import { PageNowPlaying } from './PageNowPlaying.js';
+import { useEffect, useState } from 'react';
+import { Button } from '../button.js';
 
 const meta = {
 	title: 'layouts',
@@ -71,4 +73,57 @@ export const WithoutNav: Story = {
 			</PageContent>
 		</PageRoot>
 	),
+};
+
+export const WithVirtualKeyboard: Story = {
+	render() {
+		const [keyboard, setKeyboard] = useState(false);
+
+		useEffect(() => {
+			if (keyboard) {
+				document.body.style.setProperty(
+					'--mock-virtual-keyboard-height',
+					'300px',
+				);
+			} else {
+				document.body.style.removeProperty('--mock-virtual-keyboard-height');
+			}
+		}, [keyboard]);
+
+		return (
+			<PageRoot className="w-full h-full">
+				<PageContent>
+					<div className="text-center">
+						<h1 className="text-3xl font-bold">Hello, World!</h1>
+						<p className="text-lg">This is a simple page layout.</p>
+					</div>
+					<PageNowPlaying keepAboveKeyboard>
+						<Button onClick={() => setKeyboard((v) => !v)}>
+							Toggle vkeyboard
+						</Button>
+					</PageNowPlaying>
+				</PageContent>
+				<PageNav>
+					<NavBarRoot>
+						<NavBarItem>
+							<NavBarItemIconWrapper>
+								<NavBarItemIcon name="cart" />
+							</NavBarItemIconWrapper>
+							<NavBarItemText>Item 1 long</NavBarItemText>
+						</NavBarItem>
+						<NavBarItem active={true}>
+							<NavBarItemIconWrapper>
+								<NavBarItemIcon asChild>
+									<Icon name="book" />
+								</NavBarItemIcon>
+							</NavBarItemIconWrapper>
+							<NavBarItemText>Item 2</NavBarItemText>
+							<NavBarItemPip />
+						</NavBarItem>
+					</NavBarRoot>
+				</PageNav>
+				<div className="fixed bottom-0 h-[var(--mock-virtual-keyboard-height,0)] bg-black w-full transition-height left-0 right-0" />
+			</PageRoot>
+		);
+	},
 };
