b665ce15f6b22cb8d720287e080c6733763f17a1
Grant Forrest <gaforres@gmail.com>
2025-03-14 11:52:55 -0400

    layout responsive sizing

---
+++ .changeset/wild-files-switch.md
@@ -0,0 +1,5 @@
+---
+'@a-type/ui': patch
+---
+
+layout responsive tweaks
--- src/components/layouts/PageRoot.tsx
+++ src/components/layouts/PageRoot.tsx
@@ -4,5 +4,6 @@ export const PageRoot = withClassName(
 	'div',
 	'layer-components:(flex-grow-1 flex-shrink-1 flex-basis-0 min-h-0 h-full bg-wash)',
 	'layer-components:(grid grid-areas-[content]-[nav] grid-cols-[1fr] grid-rows-[1fr] items-start justify-center)',
-	'sm:layer-responsive:(grid-areas-[gutter1_nav_content_gutter2] grid-cols-[1fr_auto_min(800px,60vw)_1fr] min-h-auto bg-wash)',
+	'sm:layer-responsive:(grid-areas-[gutter1_nav_content_gutter2] grid-cols-[max(1rem,1fr)_auto_min(800px,80vw)_max(1rem,1fr)] min-h-auto bg-wash)',
+	'lg:layer-responsive:(grid-cols-[1fr_auto_min(800px,70vw)_1fr])',
 );
