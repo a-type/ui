6af9ed50be0889da9038a1ca520b41eb1a4e0c00
Grant Forrest <gaforres@gmail.com>
2025-03-14 22:05:27 -0400

    rich text typography

---
+++ src/components/richEditor/index.tsx
@@ -0,0 +1,30 @@
+import clsx from 'clsx';
+
+export const tipTapClassName = clsx(
+	'layer-components:(w-full leading-relaxed)',
+	'layer-components:[&_.ProseMirror:focus]:(outline-none shadow-focus)',
+	'layer-components:[&_h1,h2,h3,p]:mt-0',
+	'layer-components:[&_h1]:(text-2xl font-semibold mb-md mt-xl)',
+	'layer-components:[&_h2]:(text-lg font-bold color-gray-dark color-darken-7 mb-md mt-lg)',
+	'layer-components:[&_h3]:(text-md font-500 color-gray-dark color-darken-5 mb-sm mt-md)',
+	'layer-components:[&_h4]:(text-xs color-gray-dark color-darken-3 mb-sm mt-md uppercase)',
+	'layer-components:[&_p]:(mb-md mt-0)',
+	'layer-components:[&_a]:underline',
+	'layer-components:[&_blockquote]:(border-l-4 border-l-solid border-gray pl-4 mb-md mt-md ml-md)',
+	'layer-components:[&_ul]:(list-disc pl-6 mb-md mt-md)',
+	'layer-components:[&_ol]:(list-decimal pl-6 mb-md mt-md)',
+	'layer-components:[&_li]:(mb-sm)',
+	'layer-components:[&_mark]:(bg-accent-wash bg-darken-3)',
+	'layer-components:[&_strong]:(font-semibold)',
+	'layer-components:[&_em]:(italic)',
+	'layer-components:[&_a]:(underline)',
+	'layer-components:[&_a:hover]:(underline)',
+	'layer-components:[&_a:focus]:(outline-none shadow-focus)',
+	'layer-components:[&_a:focus-visible]:(outline-none ring-inset ring-4 ring-gray)',
+	'layer-components:[&_.ProseMirror]:(bg-white rounded-[20px] px-4 py-2 shadow-sm-inset border-gray-dark border-solid border-1)',
+);
+
+export const tipTapReadonlyClassName = clsx(
+	'[&_.ProseMirror]:(bg-transparent border-none shadow-none)',
+	'[&_.ProseMirror:focus]:(outline-none shadow-none bg-transparent)',
+);
---
+++ src/components/richEditor/richEditor.stories.tsx
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import clsx from 'clsx';
+import { tipTapClassName, tipTapReadonlyClassName } from './index.js';
+
+const meta = {
+	title: 'richEditor',
+	argTypes: {
+		readonly: { control: 'boolean' },
+	},
+	parameters: {
+		controls: { expanded: true },
+	},
+} satisfies Meta<{ readonly: boolean }>;
+
+export default meta;
+
+type Story = StoryObj<{ readonly: boolean }>;
+
+export const Default: Story = {
+	render(args) {
+		return (
+			<div
+				className={clsx(
+					tipTapClassName,
+					args.readonly && tipTapReadonlyClassName,
+				)}
+			>
+				<div className="ProseMirror">
+					<p>{paragraphText}</p>
+					<h1>Heading 1</h1>
+					<p>{paragraphText}</p>
+					<h2>Heading 2</h2>
+					<p>{paragraphText}</p>
+					<p>{paragraphText}</p>
+
+					<p>{paragraphText}</p>
+
+					<blockquote>{paragraphText}</blockquote>
+					<h3>Heading 3</h3>
+					<ul>
+						<li>{paragraphText}</li>
+						<li>{paragraphText}</li>
+					</ul>
+					<p>{paragraphText}</p>
+					<ol>
+						<li>{paragraphText}</li>
+						<li>{paragraphText}</li>
+					</ol>
+					<h4>Heading 4</h4>
+					<p>{paragraphText}</p>
+					<p>
+						<mark>Highlighted</mark> text
+					</p>
+					<p>
+						<a href="#">Link</a>
+					</p>
+					<p>
+						<strong>Bold</strong> text
+					</p>
+					<p>
+						<em>Italic</em> text
+					</p>
+				</div>
+			</div>
+		);
+	},
+};
+
+const paragraphText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`;
