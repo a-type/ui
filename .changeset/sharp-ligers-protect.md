14f5f373aa55347c551792e76d8c3217dc0ac14d
Grant Forrest <gaforres@gmail.com>
2025-05-12 17:17:07 -0400

    note css tweaks

---
+++ .changeset/sharp-ligers-protect.md
@@ -0,0 +1,5 @@
+---
+'@a-type/ui': patch
+---
+
+note css tweaks
--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -8,21 +8,21 @@ export interface NoteProps extends HTMLAttributes<HTMLDivElement> {
 export function Note({ className, children, ...rest }: NoteProps) {
 	return (
 		<div className={classNames(className)} {...rest}>
-			<div className="flex flex-row ">
-				<div className="flex-1 p-2 border border-solid border-primary-dark bg-primary-wash color-black relative text-sm italic border-r-0">
+			<div className="layer-components:(flex flex-row)">
+				<div className="layer-components:(flex-1 p-2 border border-solid border-primary-dark bg-primary-wash color-black relative text-sm italic) layer-variants:border-r-0">
 					{children}
 				</div>
 				<div
-					className="flex flex-col items-stretch justify-stretch flex-[0_0_20px]"
+					className="layer-components:(flex flex-col items-stretch justify-stretch flex-[0_0_20px])"
 					aria-hidden
 				>
-					<div className="border-0 border-solid border-primary-dark border-b-1px border-l-1px flex-[0_0_20px] w-[20px] h-[20px] relative">
-						<div className="absolute w-1px bg-primary-dark h-26px rotate--45 left-9px top--4px transform-origin-cc" />
+					<div className="layer-components:(border-0 border-solid border-primary-dark flex-[0_0_20px] w-[20px] h-[20px] relative) layer-variants:(border-b-1px border-l-1px)">
+						<div className="layer-components:(absolute w-1px bg-primary-dark h-26px rotate--45 left-9px top--4px transform-origin-cc)" />
 						<div
-							className={`border-solid box-content border-transparent border-r-primary-wash border-13px w-0 h-0 rotate--45 translate--7px transform-origin-br`}
+							className={`layer-components:(border-solid box-content border-transparent border-r-primary-wash border-13px w-0 h-0 rotate--45 translate--7px transform-origin-br)`}
 						/>
 					</div>
-					<div className="bg-primary-wash flex-1 border-0 border-solid border-primary-dark border-r-1px border-b-1px" />
+					<div className="layer-components:(bg-primary-wash flex-1 border-0 border-solid border-primary-dark) layer-variants:(border-r-1px border-b-1px)" />
 				</div>
 			</div>
 		</div>
