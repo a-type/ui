f8a69c9b2d2ba49c0defd917459327eba15bd504
Grant Forrest <gaforres@gmail.com>
2025-11-02 09:44:28 -0500

    export palette names

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ export { toast } from 'react-hot-toast';
 export * from './colorMode.js';
 export * from './components/index.js';
 export * from './hooks.js';
+export {
+	paletteHues,
+	paletteNames,
+	type PaletteName,
+} from './uno/logic/color.js';
