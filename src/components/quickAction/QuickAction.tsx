153f7daadcf0c257cb8847403adac5783dba8f51
Grant Forrest <gaforres@gmail.com>
2026-01-24 12:44:15 -0500

    quick action props

--- src/components/quickAction/QuickAction.tsx
+++ src/components/quickAction/QuickAction.tsx
@@ -1,6 +1,5 @@
-import { Dialog } from '@base-ui/react/dialog';
+import { Dialog, DialogRootProps } from '@base-ui/react/dialog';
 import clsx from 'clsx';
-import { AnimatePresence, Transition } from 'motion/react';
 import { createContext, useContext, useId } from 'react';
 import { Button, ButtonProps } from '../button/Button.js';
 
@@ -8,24 +7,18 @@ const QuickActionIdContext = createContext<string>('qab');
 
 export const QuickActionRoot = ({
 	children,
-}: {
-	children: React.ReactNode;
+	...props
+}: Omit<DialogRootProps, 'children'> & {
+	children?: React.ReactNode;
 }) => {
 	const id = useId();
 	return (
-		<Dialog.Root>
+		<Dialog.Root {...props}>
 			<QuickActionIdContext.Provider value={id.replace(/:/g, '')}>
 				{children}
 			</QuickActionIdContext.Provider>
 		</Dialog.Root>
 	);
-};
-
-const transition: Transition = {
-	type: 'spring',
-	damping: 25,
-	stiffness: 300,
-	mass: 0.5,
 };
 
 export type QuickActionTriggerProps = ButtonProps;
@@ -73,30 +66,28 @@ const QuickActionContent = ({
 	const layoutId = useContext(QuickActionIdContext);
 	return (
 		<Dialog.Portal>
-			<AnimatePresence>
-				<Dialog.Popup
-					className={clsx(
-						'layer-components:(contain-layout border-1 rounded-md border-solid shadow-lg transition-all bg-white border-black)',
-						'layer-components:(fixed bottom-[anchor(bottom)] overflow-clip)',
-						{
-							'[justify-self:anchor-center]': align === 'center',
-							'left-[anchor(left)]': align === 'start',
-							'right-[anchor(right)]': align === 'end',
-						},
-						'data-[starting-style]:([will-change:width,height,transform] h-[anchor-size(height)] w-[anchor-size(width)] rounded-200px opacity-0)',
-						'layer-components:[&[data-starting-style]>*]:opacity-0',
-						'data-[ending-style]:([will-change:width,height,transform] h-[anchor-size(height)] w-[anchor-size(width)] rounded-200px opacity-80)',
-						'layer-components:[&[data-ending-style]>*]:opacity-0',
-						className,
-					)}
-					style={{
-						// @ts-ignore
-						'position-anchor': `--${layoutId}`,
-					}}
-				>
-					{children}
-				</Dialog.Popup>
-			</AnimatePresence>
+			<Dialog.Popup
+				className={clsx(
+					'layer-components:(contain-layout border-1 rounded-md border-solid shadow-lg transition-all bg-white border-black)',
+					'layer-components:(fixed bottom-[anchor(bottom)] overflow-clip)',
+					{
+						'[justify-self:anchor-center]': align === 'center',
+						'left-[anchor(left)]': align === 'start',
+						'right-[anchor(right)]': align === 'end',
+					},
+					'data-[starting-style]:([will-change:width,height,transform] h-[anchor-size(height)] w-[anchor-size(width)] rounded-200px opacity-0)',
+					'layer-components:[&[data-starting-style]>*]:opacity-0',
+					'data-[ending-style]:([will-change:width,height,transform] h-[anchor-size(height)] w-[anchor-size(width)] rounded-200px opacity-80)',
+					'layer-components:[&[data-ending-style]>*]:opacity-0',
+					className,
+				)}
+				style={{
+					// @ts-ignore
+					'position-anchor': `--${layoutId}`,
+				}}
+			>
+				{children}
+			</Dialog.Popup>
 		</Dialog.Portal>
 	);
 };
