2cd1691d915e1e98b0a491d0f39a9fba184879f4
Grant Forrest <gaforres@gmail.com>
2026-06-22 16:14:18 -0400

    auto align submit button

--- src/components/forms/SubmitButton.tsx
+++ src/components/forms/SubmitButton.tsx
@@ -1,13 +1,16 @@
+import clsx from 'clsx';
 import { useFormikContext } from 'formik';
 import { Button, ButtonProps } from '../button/Button.js';
+import cls from './SubmitButton.module.css';
 
-export function SubmitButton(props: ButtonProps) {
+export function SubmitButton({ className, ...props }: ButtonProps) {
 	const { isSubmitting, isValid } = useFormikContext();
 	return (
 		<Button
 			loading={isSubmitting}
 			disabled={!isValid}
 			emphasis="primary"
+			className={clsx(cls.root, className)}
 			{...props}
 			type="submit"
 		/>
