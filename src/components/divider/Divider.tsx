936be8148f500fe2d2a1909a12bf37c99cf7e3bc
Grant Forrest <gaforres@gmail.com>
2026-06-28 23:39:42 -0400

    fix divider

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -1,12 +1,7 @@
 import { Separator as BaseSeparator } from '@base-ui/react/separator';
-import classNames from 'clsx';
+import clsx from 'clsx';
 import { HTMLAttributes } from 'react';
-import { withClassName } from '../../hooks/withClassName.js';
-
-const DividerBase = withClassName(
-	BaseSeparator,
-	'relative h-1px w-full bg-neutral-ink',
-);
+import cls from './Divider.module.css';
 
 export interface DividerProps extends HTMLAttributes<HTMLDivElement> {
 	compensate?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
@@ -23,14 +18,12 @@ export const Divider = function Divider({
 	ref?: React.Ref<HTMLDivElement>;
 }) {
 	return (
-		<DividerBase
+		<BaseSeparator
 			ref={ref}
-			style={{
-				left: compensate ? `calc(${compensate} * 0.25rem)` : undefined,
-				width: compensate ? `calc(100% - ${compensate} * 0.5rem)` : undefined,
-			}}
 			{...props}
-			className={classNames(padded && 'my-4', className)}
+			data-padded={padded ? '' : undefined}
+			data-compensate={compensate}
+			className={clsx(cls.root, className)}
 		/>
 	);
 };
