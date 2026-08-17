f0e18a4974abf9a2a70d2e0017747477eb034058
Grant Forrest <gaforres@gmail.com>
2026-08-17 07:56:30 -0400

    improve typing of hook

--- src/hooks/useOnVisible.ts
+++ src/hooks/useOnVisible.ts
@@ -2,10 +2,18 @@ import { useEffect, useRef } from 'react';
 import { useStableCallback } from './useStableCallback.js';
 
 export function useOnVisible(onVisible?: () => void) {
-	const innerRef = useRef<HTMLDivElement>(null);
+	const innerRef = useRef<any>(null);
 
 	const stableOnVisible = useStableCallback(onVisible);
 	useEffect(() => {
+		const current = innerRef.current;
+		if (
+			!current ||
+			!(current instanceof HTMLElement || current instanceof SVGElement)
+		) {
+			return;
+		}
+
 		const observer = new IntersectionObserver((entries) => {
 			if (entries[0].isIntersecting) {
 				stableOnVisible();
